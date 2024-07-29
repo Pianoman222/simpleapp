735da8cd84cbe82f44d2d083a9d5dde0608f10ef
Real name <email@domain.com>
2024-07-29 11:39:04 -0300

    Updated App.js with new changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   // Variables
-  const title = "mami pupi222333333";
+  const title = "mami pupi22233333355555555555";
   let number1 = 10;
   let number2 = 20;
 
