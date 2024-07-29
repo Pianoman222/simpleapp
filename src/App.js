45f29372caa183187a5d1b269ad7a4c4b997b0bd
Real name <email@domain.com>
2024-07-29 09:33:27 -0300

    Initial commit with simple React app demonstrating JS concepts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  // Variables
+  const title = "Learning JavaScript with React";
+  let number1 = 10;
+  let number2 = 20;
+
+  // Operators
+  const sum = number1 + number2;
+
+  // Functions
+  function multiply(a, b) {
+    return a * b;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{title}</h1>
+        <p>Number 1: {number1}</p>
+        <p>Number 2: {number2}</p>
+        <p>Sum: {sum}</p>
+        <p>Multiplication: {multiply(number1, number2)}</p>
       </header>
     </div>
   );
